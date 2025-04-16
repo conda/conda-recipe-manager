62636db2747cb424f77a0418db01a9773daa52c0
Schuyler Martin <schuylermartin45@gmail.com>
2025-04-16 13:08:36 -0600

    Adds some hashing utilities from a CRM prototype

--- conda_recipe_manager/utils/cryptography/hashing.py
+++ conda_recipe_manager/utils/cryptography/hashing.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import hashlib
+import string
 from pathlib import Path
 from typing import Callable, Final
 
@@ -39,3 +40,43 @@ def hash_str(s: str, hash_algo: Callable[[bytes], hashlib._Hash], encoding: str 
     """
     # If the string is small enough to fit in memory, we should not need to worry about buffering it.
     return hash_algo(s.encode(encoding=encoding)).hexdigest()
+
+
+def is_valid_hex(s: str) -> bool:
+    """
+    Checks if a string is a valid hex string.
+
+    :param s: String to validate
+    :returns: True if the string is a valid hex string. False otherwise.
+    """
+    return all(c in string.hexdigits for c in s)
+
+
+def is_valid_md5(s: str) -> bool:
+    """
+    Checks if a string is a valid MD5 hash.
+
+    :param s: String to validate
+    :returns: True if the string is a valid MD5 hash. False otherwise.
+    """
+    return len(s) == 32 and is_valid_hex(s)
+
+
+def is_valid_sha256(s: str) -> bool:
+    """
+    Checks if a string is a valid SHA-256 hash.
+
+    :param s: String to validate
+    :returns: True if the string is a valid SHA-256 hash. False otherwise.
+    """
+    return len(s) == 64 and is_valid_hex(s)
+
+
+def is_valid_sha1(s: str) -> bool:
+    """
+    Checks if a string is a valid SHA-1 hash. This is used by git/GitHub.
+
+    :param s: String to validate
+    :returns: True if the string is a valid SHA-1 hash. False otherwise.
+    """
+    return len(s) == 40 and is_valid_hex(s)
