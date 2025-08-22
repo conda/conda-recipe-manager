39218f0996ece885dd26ae0a271570e79507733a
Schuyler Martin <schuyler-martin-oss@pm.me>
2025-08-22 15:18:12 -0600

    Fixes #423 (#424)
    * Attempts to fix SPDX license issue by updating the license database AND filtering deprecated names. There are some known issues still.
    
    * Fixes and expands tests. Fixes bad GPL matches from the diff algorithm
    
    * RecipeParserConvert now uses a static SPDX utils instance
    
    * Adjusts SPDX matching algorithm to better support mixed-cased strings

--- conda_recipe_manager/licenses/spdx_utils.py
+++ conda_recipe_manager/licenses/spdx_utils.py
@@ -1,6 +1,8 @@
 """
 :Description: Provides a class that reads in the SPDX licensing database file to support SPDX utilities.
 
+              This is a read-only class that cannot be modified once initialized.
+
                 SPDX Data Source (freely available for use):
                   - https://github.com/spdx/license-list-data/blob/main/json/licenses.json
 
@@ -27,6 +29,27 @@ class SpdxUtils:
     Class that provides SPDX tooling from the SPDX license database file.
     """
 
+    # Custom patch table that attempts to correct common SPDX licensing mistakes that our other methodologies cannot
+    # handle. Maps: `MISTAKE` (all uppercase) -> `Corrected`
+    _LICENSE_MATCHING_PATCH_TBL: Final[dict[str, str]] = {
+        # This commonly used name is not close enough for `difflib` to recognize
+        'BSD 2-CLAUSE "SIMPLIFIED"': "BSD-2-Clause",
+        # Some R packages use "Unlimited". This is the mapping the team agreed to use in a Slack thread.
+        "UNLIMITED": "NOASSERTION",
+    }
+
+    # There are enough GPL license variants that maintaining all of them in the patch table would be painful.
+    # So we attempt to upgrade the older names to the newer by appending the appropriate suffix. NOTE: These strings
+    # are capitalized in order to match the "normalized" look-up table.
+    _GPL_ONLY_SUFFIXES: Final[list[str]] = [
+        "-ONLY",
+        ".0-ONLY",  # In case someone has written something like "GPL 3"
+    ]
+    _GPL_OR_LATER_SUFFIXES: Final[list[str]] = [
+        "-OR-LATER",
+        ".0-OR-LATER",
+    ]
+
     def __init__(self) -> None:
         """
         Constructs a SPDX utility instance. Reads data from the JSON file provided by the module.
@@ -38,24 +61,42 @@ class SpdxUtils:
 
         # Generate a few look-up tables for license matching once during initialization for faster future look-ups.
         self._license_matching_table: dict[str, str] = {}
-        self._license_ids: set[str] = set()
+        # Matches case-insensitive license IDs to the expected ID
+        self._license_ids_normalized_table: dict[str, str] = {}
         for license_data in self._raw_spdx_data["licenses"]:
-            license_id = license_data["licenseId"]
-            license_name = license_data["name"]
+            # Filter-out deprecated licenses. Fixes #423.
+            if "isDeprecatedLicenseId" in license_data and license_data["isDeprecatedLicenseId"]:
+                continue
+
+            license_id = license_data["licenseId"].strip()
+            license_name = license_data["name"].strip()
             # SPDX IDs are unique and used for SPDX validation. Commonly recipes use variations on names or IDs, so we
             # want to map both options to the same ID.
             self._license_matching_table[license_name] = license_id
             self._license_matching_table[license_id] = license_id
-            self._license_ids.add(license_id)
+            self._license_ids_normalized_table[license_id.upper()] = license_id
 
-        # Custom patch table that attempts to correct common SPDX licensing mistakes that our other methodologies cannot
-        # handle. Maps: `MISTAKE` (all uppercase) -> `Corrected`
-        self._license_matching_patch_tbl: Final[dict[str, str]] = {
-            # This commonly used name is not close enough for `difflib` to recognize
-            'BSD 2-CLAUSE "SIMPLIFIED"': "BSD-2-Clause",
-            # Some R packages use "Unlimited". This is the mapping the team agreed to use in a Slack thread.
-            "UNLIMITED": "NOASSERTION",
-        }
+    def _match_gpl_license(self, sanitized_license: str) -> Optional[str]:
+        """
+        Attempt to upgrade GPL licenses to their newer naming schemes. Annoyingly the JSON data does not map old
+        to new names for us.
+
+        :param sanitized_license: Clean license name to start with.
+        :returns: An adjusted GPL license name, if one is matched. Otherwise, `None`.
+        """
+        # Determine if we are in the "-or-later" case.
+        # NOTE: In the future, we could look into making a network call to match old to new(?)
+        target_suffixes: Final = (
+            SpdxUtils._GPL_OR_LATER_SUFFIXES if sanitized_license[-1] == "+" else SpdxUtils._GPL_ONLY_SUFFIXES
+        )
+        sanitized_gpl_license: Final = sanitized_license[:-1] if sanitized_license[-1] == "+" else sanitized_license
+
+        for suffix in target_suffixes:
+            license_with_suffix = f"{sanitized_gpl_license}{suffix}"
+            if license_with_suffix in self._license_ids_normalized_table:
+                return self._license_ids_normalized_table[license_with_suffix]
+
+        return None
 
     def find_closest_license_match(self, license_field: str) -> Optional[str]:
         """
@@ -70,11 +111,20 @@ class SpdxUtils:
         :param license_field: License string provided by the recipe to match
         :returns: The closest matching SPDX identifier, if found
         """
-        # Short-circuit on perfect matches
-        if license_field in self._license_ids:
-            return license_field
+        sanitized_license: Final = license_field.strip().upper()
 
-        sanitized_license = license_field.strip().upper()
+        # Short-circuit on perfect matches
+        if sanitized_license in self._license_ids_normalized_table:
+            return self._license_ids_normalized_table[sanitized_license]
+
+        # Correct known commonly used licenses that can't be handled by `difflib`. NOTE: This table normalizes around
+        # upper-cased keys.
+        if sanitized_license in SpdxUtils._LICENSE_MATCHING_PATCH_TBL:
+            return SpdxUtils._LICENSE_MATCHING_PATCH_TBL[sanitized_license]
+
+        # Short-circuit on known deprecation upgrade paths.
+        if (gpl_match := self._match_gpl_license(sanitized_license)) is not None:
+            return gpl_match
 
         # TODO: Improve this logic to support SPDX expressions.
         # Don't simplify compound licenses that might get accidentally simplified
@@ -83,10 +133,6 @@ class SpdxUtils:
                 return None
         if "," in sanitized_license:
             return None
-
-        # Correct known commonly used licenses that can't be handled by `difflib`
-        if sanitized_license in self._license_matching_patch_tbl:
-            return self._license_matching_patch_tbl[sanitized_license]
 
         match_list = difflib.get_close_matches(license_field, self._license_matching_table.keys(), 1)
         if not match_list:
--- tests/license/test_spdx_utils.py
+++ tests/license/test_spdx_utils.py
@@ -16,21 +16,40 @@ from conda_recipe_manager.licenses.spdx_utils import SpdxUtils
         ("Apache License 1.0", "Apache-1.0"),
         ("Apache License 1.1", "Apache-1.1"),
         ("Apache License 2.0", "Apache-2.0"),
+        ("APaCHE LIcenSe 2.0", "Apache-2.0"),
         #### BSD ####
         ("BSD 1-Clause", "BSD-1-Clause"),
         ("BSD_2_Clause", "BSD-2-Clause"),
         ("BSD 3-Clause", "BSD-3-Clause"),
         ("BSD_3_Clause", "BSD-3-Clause"),
         #### GPL ####
-        ("GPL-2", "GPL-2.0"),
-        ("GPL-3", "GPL-3.0"),
+        ("GPL-2.0-only", "GPL-2.0-only"),
+        ("GPL-3.0-only", "GPL-3.0-only"),
+        ("AGPL-1.0-only", "AGPL-1.0-only"),
+        ("LGPL-2.1-or-later", "LGPL-2.1-or-later"),
+        # See Issue #423 for more details about converting to use `-only`/`-or-later` names.
+        ("GPL-2", "GPL-2.0-only"),
+        ("GPL-3", "GPL-3.0-only"),
+        ("GPL-2.0", "GPL-2.0-only"),
+        ("GPL-3.0", "GPL-3.0-only"),
+        ("AGPL-1.0", "AGPL-1.0-only"),
+        ("AGPL-1", "AGPL-1.0-only"),
+        ("GPL-1.0+", "GPL-1.0-or-later"),
+        ("GPL-1+", "GPL-1.0-or-later"),
+        ("LGPL-2.1+", "LGPL-2.1-or-later"),
+        ("LGPL-2.1", "LGPL-2.1-only"),
         #### Special cases that are "manually" handled outside of `difflib` ####
         ('BSD 2-Clause "SIMPLIFIED"', "BSD-2-Clause"),
+        #### Matches that should work if casing is normalized ####
+        ("gPl-2", "GPL-2.0-only"),
+        ("gpL-2.0", "GPL-2.0-only"),
+        ("LgPl-2.1+", "LGPL-2.1-or-later"),
     ],
 )
-def test_find_closest_license_match_common_misuse(license_field: str, expected: str) -> None:
+def test_find_closest_license_match_common_issues(license_field: str, expected: str) -> None:
     """
-    Validates license matching with commonly used incorrect license names
+    Validates license matching with commonly used "incorrect" license names and attempts to upgrade deprecated license
+    names.
     """
     # TODO fixture
     spdx_utils = SpdxUtils()
@@ -41,7 +60,9 @@ def test_find_closest_license_match_common_misuse(license_field: str, expected: 
     "license_field",
     [
         "foobar",
-        "batman",
+        # Batman no longer works because `Gutmann` is a legitimate license with a close-enough name. So we're stuck with
+        # the sidekick, I guess.
+        "robin",
         "fadsjkl;adshbfjkasd",
     ],
 )
