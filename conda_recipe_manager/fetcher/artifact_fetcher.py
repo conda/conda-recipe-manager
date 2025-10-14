5cdef6fc76e0bf7f2b9120e6ff3b78d4bfc948c1
Schuyler Martin <schuyler-martin-oss@pm.me>
2025-10-14 11:38:11 -0600

    Refactors future-based source artifact fetching to its own artifact-fetching-function

--- conda_recipe_manager/fetcher/artifact_fetcher.py
+++ conda_recipe_manager/fetcher/artifact_fetcher.py
@@ -4,17 +4,32 @@
 
 from __future__ import annotations
 
+import concurrent.futures as cf
+import logging
+import time
 from contextlib import ExitStack, contextmanager
-from typing import Generator, cast
+from typing import Final, Generator, cast
 
 from conda_recipe_manager.fetcher.base_artifact_fetcher import BaseArtifactFetcher
-from conda_recipe_manager.fetcher.exceptions import FetchUnsupportedError
+from conda_recipe_manager.fetcher.exceptions import FetchError, FetchUnsupportedError
 from conda_recipe_manager.fetcher.git_artifact_fetcher import GitArtifactFetcher
 from conda_recipe_manager.fetcher.http_artifact_fetcher import HttpArtifactFetcher
 from conda_recipe_manager.parser.recipe_reader import RecipeReader
 from conda_recipe_manager.parser.types import SchemaVersion
 from conda_recipe_manager.types import Primitives
 from conda_recipe_manager.utils.typing import optional_str
+
+log: Final = logging.getLogger(__name__)
+
+# Maps the `/source` recipe-section path to a corresponding recipe fetcher instance.
+FetcherTable = dict[str, BaseArtifactFetcher]
+# Maps a future to its associated `/source` recipe-section path and a fetcher instance that fetched in the future.
+FetcherFuturesTable = dict[cf.Future[None], tuple[str, BaseArtifactFetcher]]
+
+# Maximum number of retries to attempt when trying to fetch an external artifact.
+DEFAULT_RETRY_LIMIT: Final[int] = 5
+# How much longer (in seconds) we should wait per retry.
+DEFAULT_RETRY_INTERVAL: Final[int] = 10
 
 
 def _render_git_key(recipe: RecipeReader, key: str) -> str:
@@ -44,9 +59,6 @@ def _render_git_key(recipe: RecipeReader, key: str) -> str:
                 # flag in the hopes of being caught early by a unit test.
                 case _:
                     raise FetchUnsupportedError(f"The following key is not supported for git sources: {key}")
-
-
-FetcherTable = dict[str, BaseArtifactFetcher]
 
 
 @contextmanager
@@ -112,3 +124,54 @@ def from_recipe(recipe: RecipeReader, ignore_unsupported: bool = False) -> Gener
                 raise FetchUnsupportedError(f"{recipe_name} contains an unsupported source object at `{src_path}`.")
 
         yield sources
+
+
+def _fetch_archive(fetcher: BaseArtifactFetcher, retry_interval: int, retries: int) -> None:
+    """
+    Fetches the target source archive (with retries) for future use.
+
+    :param fetcher: Artifact fetching instance to use.
+    :param retry_interval: (Optional) Base quantity of time (in seconds) to wait between fetch attempts.
+    :param retries: (Optional) Number of retries to attempt. Defaults to `_RETRY_LIMIT` constant.
+    :raises FetchError: If an issue occurred while downloading or extracting the archive.
+    """
+    # NOTE: This is the most I/O-bound operation in `bump-recipe` by a country mile. At the time of writing,
+    # running this operation in the background will not make any significant improvements to performance. Every other
+    # operation is so fast in comparison, any gains would likely be lost with the additional overhead. This op is
+    # also inherently reliant on having the version change performed ahead of time. In addition, parallelizing the
+    # retries defeats the point of having a back-off timer.
+
+    for retry_id in range(1, retries + 1):
+        try:
+            log.info("Fetching artifact `%s`, attempt #%d", fetcher, retry_id)
+            fetcher.fetch()
+            return
+        except FetchError:
+            if retry_id < retries:
+                time.sleep(retry_id * retry_interval)
+
+    raise FetchError(f"Failed to fetch `{fetcher}` after {retries} retries.")
+
+
+@contextmanager
+def fetch_all_artifacts_with_retry(
+    recipe_reader: RecipeReader, retry_interval: int = DEFAULT_RETRY_INTERVAL, retries: int = DEFAULT_RETRY_LIMIT
+) -> Generator[FetcherFuturesTable]:
+    """
+    Starts a threadpool that pulls-down all source artifacts for a recipe file, with a built-in retry mechanism.
+
+    :param recipe_reader: READ-ONLY Parser instance for the target recipe. Ensuring this is a read-only parsing class
+        provides some thread safety through abusing a type checker (like `mypy`).
+    :param retry_interval: (Optional) Base quantity of time (in seconds) to wait between fetch attempts.
+    :param retries: (Optional) Number of retries to attempt. Defaults to `_RETRY_LIMIT` constant.
+    :raises FetchError: On resolving any returned future, if fetching a source artifact failed.
+    :returns: A generator containing a table that maps futures to the source artifact path in the recipe file and
+        the fetcher instance itself.
+    """
+    with from_recipe(recipe_reader, True) as fetcher_tbl:
+        with cf.ThreadPoolExecutor() as executor:
+            artifact_futures_tbl = {
+                executor.submit(_fetch_archive, fetcher, retry_interval, retries): (src_path, fetcher)
+                for src_path, fetcher in fetcher_tbl.items()
+            }
+            yield artifact_futures_tbl
